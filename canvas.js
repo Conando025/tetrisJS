331d7685aefe720c7d0035e3597b84f0fbf102d1
Conando <37964708+Conando025@users.noreply.github.com>
2021-10-08 11:56:24 +0200

    loop

--- canvas.js
+++ canvas.js
@@ -2,4 +2,9 @@ const canvas = document.getElementById('canvas');
 const size = canvas.width / 10;
 let ctx = canvas.getContext("2d");
 let tet_L = new L(ctx);
-tet_L.draw()
+
+let gameplayloop = true;
+
+while (gameplayloop) {
+	tet_L.draw();
+}
